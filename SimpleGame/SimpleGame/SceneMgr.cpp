e3f5e5e420c91921e4cb106068ec242d053595b6
Lee Do Yeon <tom212@naver.com>
2017-10-18 14:57:06 +0900

    SceneManager

--- SimpleGame/SimpleGame/SceneMgr.cpp
+++ SimpleGame/SimpleGame/SceneMgr.cpp
@@ -2,7 +2,12 @@
 #include "Object.h"
 #include "SceneMgr.h"
 
+#include <random>
+
 using namespace std;
+default_random_engine dre;
+uniform_int_distribution<> ui(0, 80);
+
 
 void SceneMgr::ObjectAdd()
 {
@@ -11,7 +16,8 @@ void SceneMgr::ObjectAdd()
 		m_objects[i] = NULL;
 		if (m_objects[i] == NULL)
 		{
-			m_objects[i] = new Object(0, 0, 0, 4, 1, 0, 1, 1);
+			// x, y, z, s, r, g, b, a
+			m_objects[i] = new Object(ui(dre), ui(dre), 1, 10, 1, 1, 1, 0);
 			m_objects[i]->ObjectInitialize(
 				m_objects[i]->GetObjectXposition(),
 				m_objects[i]->GetObjectYposition(),
